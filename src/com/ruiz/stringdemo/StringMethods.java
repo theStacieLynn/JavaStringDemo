5d91991b6dc77287f7d970161e4f900a17efde05
Stacie Ruiz <s_lynn.r@outlook.com>
2022-11-27 16:48:43 -0600

    final push

--- src/com/ruiz/stringdemo/StringMethods.java
+++ src/com/ruiz/stringdemo/StringMethods.java
@@ -12,6 +12,7 @@ public class StringMethods {
 		changeCase();
 		demoConcat();
 		demoSplitString();
+		demoCompareTo();
 
 	}
 	public static void getLength() {
@@ -85,6 +86,33 @@ public class StringMethods {
 		String vowels = "a::b::c::d:e";
 		String [] result = vowels.split("::");
 		System.out.println("result: "+Arrays.toString(result));
+		
+	}
+	public static void demoCharAt() {
+		String message = "Welcome to Java";
+		System.out.println("The first letter in this message is: "+message.charAt(0));
+	}
+	public static void demoCompareTo() {
+		String s1 = "hello";
+		String s2 = "hello";
+		String s3 = "hemlo";
+		String s4 = "flag";
+		System.out.println(s1.compareTo(s2));//0 because both are equal
+		System.out.println(s1.compareTo(s3));//-1  because "l" is only one time
+		System.out.println(s1.compareTo(s4));
+		
+		/*The above program shows the comparison between the various Strings.
+		Notice:
+		if s1 > s2, it returns a positive number.
+		if s1 < s2, it returns a negative number.
+		if s1 == s2, it returns 0.*/
+
+	}
+	public static void demoSubstring() {
+		//The substring method extracts a substring from the string and returns it.
+		//The syntax of the substring() method is
+		//stringObj.substring(int startIndex, int endIndex)
+		String str1 = "Java is fun";
 		
 	}
 }
